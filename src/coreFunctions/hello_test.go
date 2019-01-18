f480b25ec1493a17c9f8f0c148d1f0e6411951f6
lucapaterlini <support@crowdcoin.site>
2019-01-18 15:21:08 +0000

    update error

--- src/coreFunctions/hello_test.go
+++ src/coreFunctions/hello_test.go
@@ -6,7 +6,7 @@ func TestHello(t *testing.T) {
 	s, e := Hello("luca")
 	expectedAnswer := "Hello luca"
 	if e != nil {
-		t.Error("Error in TestHello")
+		t.Error("Error in TestHello",e.Error())
 	}else if s!=expectedAnswer{
 		t.Errorf("wrong message; got %s instead of %s",s,expectedAnswer)
 	}
