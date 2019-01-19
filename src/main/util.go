35c66e189ef6939eb0d95539a08cdce1a9178ea5
luca paterlini <paterlini.luca@gmail.com>
2019-01-19 10:46:17 +0000

    urlQueryUnescape improvement

--- src/main/util.go
+++ src/main/util.go
@@ -19,15 +19,20 @@ func logPanics(function fasthttp.RequestHandler)fasthttp.RequestHandler{
 	}
 }
 
+
 func middlewareEndpoint(ctx *fasthttp.RequestCtx,f func(map[string]interface{})string){
-	d := calc.ParseQueryStringToDict(ctx.QueryArgs().String())
-	key:=string(ctx.Path())+ctx.QueryArgs().String()
-	if _, found := cache.Get(key); !found {
-		cache.Set(key, f(d), gocache.DefaultExpiration)
+	query,err := url.QueryUnescape(ctx.QueryArgs().String())
+	if err==nil {
+		d := calc.ParseQueryStringToDict(query)
+		key := string(ctx.Path()) + ctx.QueryArgs().String()
+		if _, found := cache.Get(key); !found {
+			cache.Set(key, f(d), gocache.DefaultExpiration)
+		}
+		response, _ := cache.Get(key)
+		_, err = fmt.Fprint(ctx, fmt.Sprintf("%v", response))
 	}
-	response,_:= cache.Get(key)
-	_,err :=fmt.Fprint(ctx, fmt.Sprintf("%v", response))
 	if err== nil {ctx.SetStatusCode(fasthttp.StatusOK)
 	} else {ctx.SetStatusCode(fasthttp.StatusInternalServerError)}
 }
+
 
